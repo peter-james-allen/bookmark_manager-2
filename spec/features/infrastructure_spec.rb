a425c7af0b93d6807a162fccf9aa8f9414e7aad2
charlierdm <charlie.davismiller@gmail.com>
2021-02-23 16:06:51 +0000

    updated viewing_bookmarks_spec.rb

--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -6,12 +6,3 @@ feature 'Test infrastructure' do
     expect(page).to have_content 'Bookmark Manager'
   end
 end
-
-feature 'Viewing Bookmarks' do
-  scenario 'displays the bookmarks' do
-    visit('/')
-    click_button 'Bookmarks'
-    expect(page).to have_content 'http://www.google.com'
-    expect(page).to have_content 'http://www.makersacademy.com'
-  end
-end
